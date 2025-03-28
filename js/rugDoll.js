ab3a352e6968aefd9d679bace9c890135905cc3f
Krunalsinh <krunalsinh7718@gmail.com>
2025-03-28 13:32:18 +0530

    changes

--- js/rugDoll.js
+++ js/rugDoll.js
@@ -95,150 +95,407 @@ var obstacles = Composites.stack(300, 0, 15, 3, 10, 10, function (x, y, column) 
 
 });
 
-// Composite.add(world, [obstacles]);
-
-// function createDoll(x, y, scale) {
-
-//     const size = {
-//         head: {
-//             height: 50,
-//             width: 50,
-//         },
-//         chest: {
-//             height: 200,
-//             width: 150,
-//         },
-//         handLegUpper: {
-//             height: 25,
-//             width: 100
-//         },
-//         handLegLower: {
-//             height: 25,
-//             width: 100
-//         }
-//     }
-//     const collisionFilter = Body.nextGroup(true);
-//     const dollHead = Bodies.rectangle(
-//         x,
-//         y,
-//         size.head.width,
-//         size.head.height,
-//         {
-//             label: 'head',
-//             collisionFilter: {
-//                 group: collisionFilter
-//             },
-//             chamfer: {
-//                 radius: [15 * 0.5, 15 * 0.5, 15 * 0.5, 15 * 0.5]
-//             },
-//             render: {
-//                 fillStyle: '#FFBC42'
-//             }
-//         })
-
-//     const dollChest = Bodies.rectangle(
-//         x - size.head.width,
-//         y + size.head.height,
-//         size.chest.width,
-//         size.chest.height,
-//         {
-//             label: 'chest',
-//             collisionFilter: {
-//                 group: collisionFilter
-//             },
-//             chamfer: {
-//                 radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
-//             },
-//             render: {
-//                 fillStyle: '#FFBC42'
-//             }
-//         })
-
-//     const hedChestConstraint = Constraint.create({
-//         bodyA: dollHead,
-//         pointA: {
-//             x: dollHead.position.x + size.head.width / 2,
-//             y: dollHead.position.y + size.head.height
-//         },
-//         bodyB: dollChest,
-//         pointB: {
-//             x: dollChest.position.x + size.chest.width / 2,
-//             y: dollHead.position.y + size.head.height
-//         },
-//         stiffness: 0.6,
-//         render: {
-//             visible: false
-//         }
-//     })
-
-//     return Composite.create({
-//         label: "dollWrapper",
-//         bodies: [
-//             dollHead, dollChest
-//         ],
-//         constraints: [
-//             hedChestConstraint
-//         ]
-//     })
-// }
+Composite.add(world, [obstacles]);
 
 function createDoll(x, y, scale) {
-    var head = Bodies.rectangle(x, y - 60 * scale, 34 * scale, 40 * scale, {
-        label: 'head',
-        collisionFilter: {
-            group: Body.nextGroup(true)
-        },
-        chamfer: {
-            radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
-        },
-        render: {
-            fillStyle: '#FFBC42'
-        }
-    });
 
-    var chest = Bodies.rectangle(x, y, 55 * scale, 80 * scale, {
-        label: 'chest',
-        collisionFilter: {
-            group: Body.nextGroup(true)
+    const size = {
+        head: {
+            height: 50,
+            width: 50,
         },
-        chamfer: {
-            radius: [20 * scale, 20 * scale, 26 * scale, 26 * scale]
+        chest: {
+            height: 200,
+            width: 150,
         },
-        render: {
-            fillStyle: '#E0A423'
-        }
-    });
+        handLeg: {
+            height: 35,
+            width: 80
+        },
 
-    var headContraint = Constraint.create({
-        bodyA: head,
+    }
+    const dollHead = Bodies.rectangle(
+        x,
+        y + size.head.height / 2,
+        size.head.width,
+        size.head.height,
+        {
+            label: 'head',
+            isStatic: false,
+            collisionFilter: {
+                group: Body.nextGroup(true)
+            },
+            chamfer: {
+                radius: [15 * 0.5, 15 * 0.5, 15 * 0.5, 15 * 0.5]
+            },
+            render: {
+                fillStyle: '#FFBC42'
+            }
+        })
+
+    const dollChest = Bodies.rectangle(
+        x,
+        y + size.chest.height / 2 + size.head.height,
+        size.chest.width,
+        size.chest.height,
+        {
+            label: 'chest',
+            isStatic: false,
+            collisionFilter: {
+                group: Body.nextGroup(true)
+            },
+            chamfer: {
+                radius: [30 * scale, 30 * scale, 30 * scale, 30 * scale]
+            },
+            render: {
+                fillStyle: 'red'
+            }
+        })
+
+    const hedChestConstraint = Constraint.create({
+        bodyA: dollHead,
         pointA: {
             x: 0,
-            y: 25 * scale
+            y: size.head.height / 2
         },
+        bodyB: dollChest,
         pointB: {
             x: 0,
-            y: -35 * scale
+            y: -size.chest.height / 2
         },
-        bodyB: chest,
         stiffness: 0.6,
         render: {
             visible: false
         }
-    });
+    })
 
-    var person = Composite.create({
+    const leftUpperHand = Bodies.rectangle(
+        x - size.handLeg.width / 2 - size.chest.width / 2,
+        y - (size.handLeg.height / 2) + size.head.height + size.handLeg.height,
+        size.handLeg.width,
+        size.handLeg.height,
+        {
+            label: 'upperHand',
+            isStatic: false,
+            collisionFilter: {
+                group: Body.nextGroup(true)
+            },
+            chamfer: {
+                radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
+            },
+            render: {
+                fillStyle: 'blue'
+            }
+        })
+
+    const leftUpperHandConstraint = Constraint.create({
+        bodyA: leftUpperHand,
+        pointA: {
+            x: size.handLeg.width / 2,
+            y: 0
+        },
+        bodyB: dollChest,
+        pointB: {
+            x: -size.chest.width / 2,
+            y: -size.chest.height / 2 + size.handLeg.height / 2
+        },
+        stiffness: 0.6,
+        render: {
+            visible: false
+        }
+    })
+
+    const leftLowerHand = Bodies.rectangle(
+        x - size.chest.width / 2 - size.handLeg.width * 1.5,
+        y - (size.handLeg.height / 2) + size.head.height + size.handLeg.height,
+        size.handLeg.width,
+        size.handLeg.height,
+        {
+            label: 'upperHand',
+            isStatic: false,
+            collisionFilter: {
+                group: Body.nextGroup(true)
+            },
+            chamfer: {
+                radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
+            },
+            render: {
+                fillStyle: 'yellow'
+            }
+        })
+
+    const leftLowerHandConstraint = Constraint.create({
+        bodyA: leftLowerHand,
+        pointA: {
+            x: size.handLeg.width / 2,
+            y: 0
+        },
+        bodyB: leftUpperHand,
+        pointB: {
+            x: -size.handLeg.width / 2,
+            y: 0
+        },
+        stiffness: 0.6,
+        render: {
+            visible: false
+        }
+    })
+
+
+    const leftUpperLeg = Bodies.rectangle(
+        x - size.handLeg.width / 2 - size.chest.width / 2,
+        y + size.chest.height + size.head.height - size.handLeg.height / 2,
+        size.handLeg.width,
+        size.handLeg.height,
+        {
+            label: 'upperLeg',
+            isStatic: false,
+            collisionFilter: {
+                group: Body.nextGroup(true)
+            },
+            chamfer: {
+                radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
+            },
+            render: {
+                fillStyle: 'blue'
+            }
+        })
+
+    const leftUpperLegConstraint = Constraint.create({
+        bodyA: leftUpperLeg,
+        pointA: {
+            x: size.handLeg.width / 2,
+            y: 0
+        },
+        bodyB: dollChest,
+        pointB: {
+            x: -size.chest.width / 2,
+            y: size.chest.width / 2
+        },
+        stiffness: 0.6,
+        render: {
+            visible: false
+        }
+    })
+
+    const leftLowerLeg = Bodies.rectangle(
+        x - size.chest.width / 2 - size.handLeg.width * 1.5,
+        y + size.chest.height + size.head.height - size.handLeg.height / 2,
+        size.handLeg.width,
+        size.handLeg.height,
+        {
+            label: 'upperLeg',
+            isStatic: false,
+            collisionFilter: {
+                group: Body.nextGroup(true)
+            },
+            chamfer: {
+                radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
+            },
+            render: {
+                fillStyle: 'yellow'
+            }
+        })
+
+    const leftLowerLegConstraint = Constraint.create({
+        bodyA: leftLowerLeg,
+        pointA: {
+            x: size.handLeg.width / 2,
+            y: 0
+        },
+        bodyB: leftUpperLeg,
+        pointB: {
+            x: -size.handLeg.width / 2,
+            y: 0
+        },
+        stiffness: 0.6,
+        render: {
+            visible: false
+        }
+    })
+
+    //------------------------------------------------------------------
+
+
+    const rightUpperHand = Bodies.rectangle(
+        x + size.handLeg.width / 2 + size.chest.width / 2,
+        y - (size.handLeg.height / 2) + size.head.height + size.handLeg.height,
+        size.handLeg.width,
+        size.handLeg.height,
+        {
+            label: 'upperHand',
+            isStatic: false,
+            collisionFilter: {
+                group: Body.nextGroup(true)
+            },
+            chamfer: {
+                radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
+            },
+            render: {
+                fillStyle: 'blue'
+            }
+        })
+
+    const rightUpperHandConstraint = Constraint.create({
+        bodyA: rightUpperHand,
+        pointA: {
+            x: -size.handLeg.width / 2,
+            y: 0
+        },
+        bodyB: dollChest,
+        pointB: {
+            x: size.chest.width / 2,
+            y: -size.chest.height / 2 + size.handLeg.height / 2
+        },
+        stiffness: 0.6,
+        render: {
+            visible: false
+        }
+    })
+
+    const rightLowerHand = Bodies.rectangle(
+        x + size.chest.width / 2 + size.handLeg.width * 1.5,
+        y - (size.handLeg.height / 2) + size.head.height + size.handLeg.height,
+        size.handLeg.width,
+        size.handLeg.height,
+        {
+            label: 'upperHand',
+            isStatic: false,
+            collisionFilter: {
+                group: Body.nextGroup(true)
+            },
+            chamfer: {
+                radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
+            },
+            render: {
+                fillStyle: 'yellow'
+            }
+        })
+
+    const rightLowerHandConstraint = Constraint.create({
+        bodyA: rightLowerHand,
+        pointA: {
+            x: -size.handLeg.width / 2,
+            y: 0
+        },
+        bodyB: rightUpperHand,
+        pointB: {
+            x: size.handLeg.width / 2,
+            y: 0
+        },
+        stiffness: 0.6,
+        render: {
+            visible: false
+        }
+    })
+
+
+    const rightUpperLeg = Bodies.rectangle(
+        x + size.handLeg.width / 2 + size.chest.width / 2,
+        y + size.chest.height + size.head.height - size.handLeg.height / 2,
+        size.handLeg.width,
+        size.handLeg.height,
+        {
+            label: 'upperLeg',
+            isStatic: false,
+            collisionFilter: {
+                group: Body.nextGroup(true)
+            },
+            chamfer: {
+                radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
+            },
+            render: {
+                fillStyle: 'blue'
+            }
+        })
+
+    const rightUpperLegConstraint = Constraint.create({
+        bodyA: rightUpperLeg,
+        pointA: {
+            x: -size.handLeg.width / 2,
+            y: 0
+        },
+        bodyB: dollChest,
+        pointB: {
+            x: size.chest.width / 2,
+            y: size.chest.width / 2
+        },
+        stiffness: 0.6,
+        render: {
+            visible: false
+        }
+    })
+
+    const rightLowerLeg = Bodies.rectangle(
+        x + size.chest.width / 2 + size.handLeg.width * 1.5,
+        y + size.chest.height + size.head.height - size.handLeg.height / 2,
+        size.handLeg.width,
+        size.handLeg.height,
+        {
+            label: 'upperLeg',
+            isStatic: false,
+            collisionFilter: {
+                group: Body.nextGroup(true)
+            },
+            chamfer: {
+                radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
+            },
+            render: {
+                fillStyle: 'yellow'
+            }
+        })
+
+    const rightLowerLegConstraint = Constraint.create({
+        bodyA: rightLowerLeg,
+        pointA: {
+            x: -size.handLeg.width / 2,
+            y: 0
+        },
+        bodyB: rightUpperLeg,
+        pointB: {
+            x: size.handLeg.width / 2,
+            y: 0
+        },
+        stiffness: 0.6,
+        render: {
+            visible: false
+        }
+    })
+
+
+
+    let person = Composite.create({
         bodies: [
-            chest, head
+            dollChest,
+            dollHead,
+            leftUpperHand,
+            leftLowerHand,
+            leftUpperLeg,
+            leftLowerLeg,
+            rightUpperHand,
+            rightLowerHand,
+            rightUpperLeg,
+            rightLowerLeg
         ],
         constraints: [
-            headContraint
+            hedChestConstraint,
+            leftUpperHandConstraint,
+            leftLowerHandConstraint,
+            leftUpperLegConstraint,
+            leftLowerLegConstraint,
+            rightUpperHandConstraint,
+            rightLowerHandConstraint,
+            rightUpperLegConstraint,
+            rightLowerLegConstraint
         ]
-    });
+    })
 
     return person;
 }
 
-const doll = createDoll(800, 200, 1);
+
+
+const doll = createDoll(200, -300, 1);
 
 Composite.add(world, doll);
 
@@ -310,17 +567,34 @@ Events.on(engine, 'afterUpdate', function (event) {
         }
     }
 
-    // for (var i = 0; i < obstacles.bodies.length; i += 1) {
-    //     var body = obstacles.bodies[i], bounds = body.bounds;;
+    for (var i = 0; i < obstacles.bodies.length; i += 1) {
+        var body = obstacles.bodies[i], bounds = body.bounds;;
 
-    //     if (bounds.min.y > render.bounds.max.y + 100) {
-    //         Body.translate(body, {
-    //             x: -bounds.min.x,
-    //             y: -render.bounds.max.y - 300
-    //         });
-    //     }
+        if (bounds.min.y > render.bounds.max.y + 100) {
+            Body.translate(body, {
+                x: -bounds.min.x,
+                y: -render.bounds.max.y - 300
+            });
+        }
 
-    // }
+    }
+
+    console.log(doll);
+    
+    for (i = 0; i < doll.bodies.length; i += 1) {
+        var ragdoll = doll.bodies[i],
+        bounds = ragdoll.bounds;
+        
+        console.log(bounds);
+
+        // move ragdolls back to the top of the screen
+        if (bounds.min.y > render.bounds.max.y + 500) {
+            Composite.translate(doll, {
+                x:  -render.bounds.max.x / 1.5,
+                y: -render.bounds.max.y - 500
+            });
+        }
+    }
 
 })
 
